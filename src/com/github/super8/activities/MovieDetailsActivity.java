1f5b1b93d9e3163b552f33b6584830daeaa2149b
Matthias Kaeppler <m.kaeppler@gmail.com>
2012-05-10 22:45:39 +0200

    Rename to MovieDetailsActivity

--- src/com/github/super8/activities/MovieDetailsActivity.java
+++ src/com/github/super8/activities/MovieDetailsActivity.java
@@ -3,7 +3,7 @@ package com.github.super8.activities;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MovieActivity extends Activity {
+public class MovieDetailsActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
